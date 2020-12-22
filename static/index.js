66bd4a3980eb114992c6a970ec76ad1f413e3f8a
Fabián Villena <villenafabian@gmail.com>
2020-12-21 22:13:51 -0300

    diseases and body parts

--- static/index.js
+++ static/index.js
@@ -35,60 +35,71 @@ $(document).ready(function() {
                 bgColor: '#7fa2ff',
                 // Use a slightly darker version of the bgColor for the border
                 borderColor: 'darken'
-        } ]
+        },
+        {
+            type   : 'Body_Part',
+            /* The labels are used when displaying the annotion, in this case
+                we also provide a short-hand "Per" for cases where
+                abbreviations are preferable */
+            labels : ['Body Part', 'BP'],
+            // Blue is a nice colour for a person?
+            bgColor: '#9CFFD9',
+            // Use a slightly darker version of the bgColor for the border
+            borderColor: 'darken'
+    }
+     ]
     };
     
     var docData = {
-        "entities": [
-            // [
-            //     "T1",
-            //     "Disease",
-            //     [
-            //         [
-            //             18,
-            //             36
-            //         ]
-            //     ]
-            // ]
-        ],
-        "labels": [
-            "O",
-            "O",
-            "O",
-            "B-Disease",
-            "I-Disease",
-            "I-Disease"
-        ],
-        "tagged_string": "el paciente tiene cáncer <B-Disease> de <I-Disease> tiroides <I-Disease>",
+        "entities": [],
         "text": "",
-        "tokens": [
-            "el",
-            "paciente",
-            "tiene",
-            "cáncer",
-            "de",
-            "tiroides"
-        ]
     }
-    
+
     var liveDispatcher = Util.embed('brat',
                 collData,
                 docData,
                 undefined);
-
+   
     
     function annotate(text) {
         $("#spinner").show();
         $("#button").addClass('disabled');
+        var brat_data = {
+            text : "",
+            entities : []
+        }
         $.ajax("https://pln.cmm.uchile.cl/clinical-ner/diseases", {
+            data : JSON.stringify({"text":text}),
+            contentType : 'application/json',
+            type : 'POST',
+        }).success(function (data) {
+            brat_data = {
+                text : data.text,
+                entities : data.entities
+            }
+            brat_data.entities.push(...data.entities)
+            liveDispatcher.post("requestRenderData", [brat_data]);
+            $.ajax("https://pln.cmm.uchile.cl/clinical-ner/body_parts", {
             data : JSON.stringify({"text":text}),
             contentType : 'application/json',
             type : 'POST',
         }).success(function (data) {
             $("#spinner").hide();
             $("#button").removeClass('disabled');
-            liveDispatcher.post("requestRenderData", [data]);
+            brat_data = {
+                text : data.text,
+                entities : brat_data.entities.concat(data.entities)
+            }
+
+            brat_data.entities.forEach(function(part, index) {
+                this.entities[index][0] = "T"+(index+1);
+              }, brat_data); // use arr as this
+              console.log(brat_data)
+
+            liveDispatcher.post("requestRenderData", [brat_data]);
         })
+        })
+        
     }
     
     $("#button").click(function () {
